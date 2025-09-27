da111f438204628a832cd321f032625533ae923e
Gshadow2005 <aninoglenn@gmail.com>
2025-09-27 23:28:23 +0800

    update Ewallet to use MainLayoutCard for consistency

--- client/src/components/e-wallet/Overview/Overview.tsx
+++ client/src/components/e-wallet/Overview/Overview.tsx
@@ -74,7 +74,7 @@ const Overview: React.FC = () => {
   ];
 
   return (
-    <div className="space-y-6">
+    <div className="space-y-6 mt-6">
       {/* Main Stats using LayoutCard directly */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
         <LayoutCard className="bg-blue-500 border-gray-500 min-h-[120px]">
