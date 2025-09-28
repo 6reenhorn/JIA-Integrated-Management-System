3bbca3439cb811d28359a7d9b14334c97fd2a44a
6reenhorn <coding.cyrilj777@gmail.com>
2025-09-28 23:12:10 +0800

    Attendance datepicker update

--- client/src/components/employees/attendance/AttendanceFilters.tsx
+++ client/src/components/employees/attendance/AttendanceFilters.tsx
@@ -2,6 +2,41 @@ import React, { useState, useRef, useEffect } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
+const CustomDateInput = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>((props, ref) => (
+  <div className="relative">
+    <input
+      {...props}
+      ref={ref}
+      placeholder="yyyy-MM-dd"
+      className={`${props.className} pr-10`}
+    />
+    <svg
+      fill="#000000"
+      height="20px"
+      width="20px"
+      version="1.1"
+      id="Layer_1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 490 490"
+      stroke="#000000"
+      className="absolute right-2 top-1/2 transform -translate-y-1/2 pointer-events-none"
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+      <g id="SVGRepo_iconCarrier">
+        <g>
+          <g>
+            <g>
+              <path d="M480,50h-55V10c0-5.523-4.477-10-10-10h-60c-5.523,0-10,4.477-10,10v40h-60V10c0-5.523-4.477-10-10-10h-60 c-5.523,0-10,4.477-10,10v40h-60V10c0-5.523-4.477-10-10-10H75c-5.523,0-10,4.477-10,10v40H10C4.477,50,0,54.477,0,60v95h490V60 C490,54.477,485.523,50,480,50z M125,80H85V20h40V80z M265,80h-40V20h40V80z M405,80h-40V20h40V80z"></path>
+              <path d="M0,480c0,5.523,4.477,10,10,10h470c5.523,0,10-4.477,10-10V177H0V480z M362.5,214c0-4.142,3.358-7.5,7.5-7.5h50 c4.142,0,7.5,3.358,7.5,7.5v50c0,4.142-3.358,7.5-7.5,7.5h-50c-4.142,0-7.5-3.358-7.5-7.5V214z M362.5,304 c0-4.142,3.358-7.5,7.5-7.5h50c4.142,0,7.5,3.358,7.5,7.5v50c0,4.142-3.358,7.5-7.5,7.5h-50c-4.142,0-7.5-3.358-7.5-7.5V304z M362.5,394c0-4.142,3.358-7.5,7.5-7.5h50c4.142,0,7.5,3.358,7.5,7.5v50c0,4.142-3.358,7.5-7.5,7.5h-50 c-4.142,0-7.5-3.358-7.5-7.5V394z M262.5,214c0-4.142,3.358-7.5,7.5-7.5h50c4.142,0,7.5,3.358,7.5,7.5v50 c0,4.142-3.358,7.5-7.5,7.5h-50c-4.142,0-7.5-3.358-7.5-7.5V214z M262.5,304c0-4.142,3.358-7.5,7.5-7.5h50 c4.142,0,7.5,3.358,7.5,7.5v50c0,4.142-3.358,7.5-7.5,7.5h-50c-4.142,0-7.5-3.358-7.5-7.5V304z M262.5,394 c0-4.142,3.358-7.5,7.5-7.5h50c4.142,0,7.5,3.358,7.5,7.5v50c0,4.142-3.358,7.5-7.5,7.5h-50c-4.142,0-7.5-3.358-7.5-7.5V394z M162.5,214c0-4.142,3.358-7.5,7.5-7.5h50c4.142,0,7.5,3.358,7.5,7.5v50c0,4.142-3.358,7.5-7.5,7.5h-50 c-4.142,0-7.5-3.358-7.5-7.5V214z M162.5,304c0-4.142,3.358-7.5,7.5-7.5h50c4.142,0,7.5,3.5,7.5,7.5v50 c0,4.142-3.358,7.5-7.5,7.5h-50c-4.142,0-7.5-3.358-7.5-7.5V304z M162.5,394c0-4.142,3.358-7.5,7.5-7.5h50 c4.142,0,7.5,3.358,7.5,7.5v50c0,4.142-3.358,7.5-7.5,7.5h-50c-4.142,0-7.5-3.358-7.5-7.5V394z M62.5,214 c0-4.142,3.358-7.5,7.5-7.5h50c4.142,0,7.5,3.358,7.5,7.5v50c0,4.142-3.358,7.5-7.5,7.5H70c-4.142,0-7.5-3.358-7.5-7.5V214z M62.5,304c0-4.142,3.358-7.5,7.5-7.5h50c4.142,0,7.5,3.358,7.5,7.5v50c0,4.142-3.358,7.5-7.5,7.5H70c-4.142,0-7.5-3.358-7.5-7.5 V304z M62.5,394c0-4.142,3.358-7.5,7.5-7.5h50c4.142,0,7.5,3.358,7.5,7.5v50c0,4.142-3.358,7.5-7.5,7.5H70 c-4.142,0-7.5-3.358-7.5-7.5V394z"></path>
+            </g>
+          </g>
+        </g>
+      </g>
+    </svg>
+  </div>
+));
+
 interface DateRange {
   start: Date;
   end: Date;
@@ -155,7 +190,7 @@ const AttendanceFilters: React.FC<AttendanceFiltersProps> = ({
       <style dangerouslySetInnerHTML={{ __html: `.wide-calendar { width: 100% !important; }` }} />
       <div className="flex flex-col gap-4 p-4 bg-white rounded-md shadow-md w-96">
       {/* Filter Type Tabs */}
-      <div className="flex border-b">
+      <div className="flex border-b-[2px] border-[#E5E7EB]">
         {(['preset', 'relative', 'custom'] as const).map((type) => (
           <button
             key={type}
@@ -280,7 +315,8 @@ const AttendanceFilters: React.FC<AttendanceFiltersProps> = ({
             <DatePicker
               selected={customStart ? new Date(customStart) : null}
               onChange={(date: Date | null) => onCustomStartChange(date ? date.toISOString().split('T')[0] : '')}
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+              customInput={<CustomDateInput />}
+              className="w-full px-4 py-[7px] border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
               dateFormat="yyyy-MM-dd"
               popperClassName="datepicker-below-center"
               popperPlacement="bottom"
@@ -290,7 +326,8 @@ const AttendanceFilters: React.FC<AttendanceFiltersProps> = ({
             <DatePicker
               selected={customEnd ? new Date(customEnd) : null}
               onChange={(date: Date | null) => onCustomEndChange(date ? date.toISOString().split('T')[0] : '')}
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+              customInput={<CustomDateInput />}
+              className="w-full px-4 py-[7px] border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
               dateFormat="yyyy-MM-dd"
               popperClassName="datepicker-below-center"
               popperPlacement="bottom"
