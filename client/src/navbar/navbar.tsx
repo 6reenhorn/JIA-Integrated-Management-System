5cc11409122fcd63f9299af5cde66a9f234e6d1b
piaamarie <sophiaflrs9@gmail.com>
2025-11-14 15:39:41 +0800

    Sidebar and Navbar Color Update

--- client/src/navbar/navbar.tsx
+++ client/src/navbar/navbar.tsx
@@ -2,7 +2,7 @@ import CustomTitleBar from "../components/common/CustomTitleBar";
 
 const Navbar = () => {
     return (
-        <div className="bg-[#00355D] w-full border-b border-[#E9EBF080]">
+        <div className="bg-[#02367b] w-full border-b border-[#E9EBF080]">
             <CustomTitleBar />
         </div>
     );
