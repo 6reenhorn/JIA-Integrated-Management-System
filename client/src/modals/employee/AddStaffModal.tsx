f9f5846f7265402be9771e4239feb6ea06b62179
6reenhorn <coding.cyrilj777@gmail.com>
2025-12-14 13:23:00 +0800

    Altered remot db

--- client/src/modals/employee/AddStaffModal.tsx
+++ client/src/modals/employee/AddStaffModal.tsx
@@ -886,12 +886,12 @@ const AddStaffModal = ({
       </div>
       <div className="w-full flex justify-end gap-2 mt-4 text-[12px] font-bold">
         <button 
-          className="border border-gray-300 hover:bg-gray-200 rounded-md px-3 py-1"
+          className="border border-gray-300 hover:bg-gray-200 rounded-md px-3 py-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
           onClick={handleClose}>
           Cancel
         </button>
         <button
-          className={`border border-gray-300 rounded-md px-3 py-[6px] text-white ${isFormValid ? 'bg-[#02367B] hover:bg-[#1C4A9E]' : 'bg-gray-400 cursor-not-allowed'}`}
+          className={`border border-gray-300 rounded-md px-3 py-[6px] text-white focus:outline-none focus:ring-2 focus:ring-blue-500 ${isFormValid ? 'bg-[#02367B] hover:bg-[#1C4A9E]' : 'bg-gray-400 cursor-not-allowed'}`}
           onClick={() => {
             if (!isSaving && onAddEmployee) {
               // Validate form before submission
